9b6d173e149705bc820080e1674823e478310e60
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-08-18 21:56:38 +0700

    update DoctorProfile model to match improved table

--- app/Models/DoctorProfile.php
+++ app/Models/DoctorProfile.php
@@ -8,7 +8,13 @@ class DoctorProfile extends Model
 {
     protected $fillable = [
         'user_id',
+        'full_name',
+        'license_number',
+        'phone',
+        'department',
         'specialization_id',
+        'room_number',
+        'status',
         'schedule'
     ];
 
