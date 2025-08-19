46f021fa9e7064b6c6e482551c01aac14c2477b9
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-08-19 19:18:36 +0700

    refactor: merge medicines table migrations

--- database/migrations/2025_08_19_115413_create_medicines_tables.php
+++ database/migrations/2025_08_19_115413_create_medicines_tables.php
@@ -11,16 +11,34 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('medicine_forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('medicine_routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('drug_classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        }); 
+
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('generic_name');
-            $table->string('drug_class');
+            $table->foreignId('drug_class_id')->constrained();
             $table->foreignId('medicine_form_id')->constrained();
             $table->string('strength');
             $table->foreignId('medicine_route_id')->constrained();
             $table->string('unit');
-            $table->integer('stock');
+            $table->integer('stock')->default(0);
             $table->decimal('price');
             $table->string('batch_number');
             $table->date('expiry_date');
@@ -35,5 +53,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('medicines');
+        Schema::dropIfExists('drug_classes');
+        Schema::dropIfExists('medicine_forms');
+        Schema::dropIfExists('medicine_routes');
     }
 };
