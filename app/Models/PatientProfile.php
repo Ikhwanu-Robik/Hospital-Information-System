2411bb5c4fb7414922a62fd6bf7e98d69582cf3a
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-08-20 19:07:56 +0700

    fix: patient_profiles model has unconventional name

--- app/Models/PatientProfile.php
+++ app/Models/PatientProfile.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Patient extends Model
+class PatientProfile extends Model
 {
     protected $fillable = [
         'user_id',
