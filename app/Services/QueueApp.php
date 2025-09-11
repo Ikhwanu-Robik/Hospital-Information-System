9c73d25d3e5ded744afd2b2dd012b512087b7d29
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-09-12 05:28:34 +0700

    feat: add queue locket balancer algorithm
    The queue locket balancer make sure prioritizes the locket and doctor with the least queue to be chosen.

--- app/Services/QueueApp.php
+++ app/Services/QueueApp.php
@@ -9,28 +9,86 @@ use App\Models\DoctorProfile;
 
 class QueueApp
 {
+    public $specialization;
+    public $locket;
+    public $doctor;
+    public $previousQueue;
+
     public function putInQueue($patient, $specialization): string
     {
+        $this->specialization = $specialization;
 
-        $locket = Locket::get()->random();
-        $doctor = DoctorProfile::where('specialization_id', $specialization->id)
-            ->get()->random();
+        $specializationHasQueue = CheckUpQueue::with('doctorProfile.specialization')
+            ->get()->where('doctorProfile.specialization.id', $this->specialization->id)
+            ->isNotEmpty();
 
-        $isDoctorBusy = CheckUpQueue::where('doctor_profile_id', $doctor->id)->exists();
+        if ($specializationHasQueue) {
+            // find number of queues owned by each locket that have queue
+            $queueLockets = CheckUpQueue::selectRaw('locket_id, COUNT(locket_id) AS locket_count')
+                ->whereHas('doctorProfile.specialization', function ($query) {
+                    $query->where('id', $this->specialization->id);
+                })
+                ->with('locket')
+                ->groupBy('locket_id')
+                ->get();
 
-        $queue = CheckUpQueue::where('locket_id', $locket->id)->latest()->first();
+            // if only some locket have queue
+            if ($queueLockets->count() != Locket::count('id')) {
+                // choose one of the missing lockets to use
+                $usedLocketIds = $queueLockets->pluck('locket_id');
+                $freeLockets = Locket::whereNotIn('id', $usedLocketIds)->get();
+                $this->locket = $freeLockets->random();
+                // note to self :
+                // it is true that this locket is the only one present in the queue
+                // if we only consider the chosen specialization, that is.
+                // There might be another specialization with your random locket
+                // so it is necessary to get the previous queue over here as well
+                $this->previousQueue = CheckUpQueue::where('locket_id', $this->locket->id)->latest()->first();
+            } else {
+                // choose the locket with the least locket_count
+                $this->locket = $queueLockets->sortBy('locket_count')->first()->locket;
+                $this->previousQueue = CheckUpQueue::where('locket_id', $this->locket->id)->latest()->get()->first();
+            }
+
+            // find number of queues owned by each doctor that have queue
+            $queueDoctors = CheckUpQueue::selectRaw('doctor_profile_id, COUNT(doctor_profile_id) AS queues_count')
+                ->whereHas('doctorProfile.specialization', function ($query) {
+                    $query->where('id', $this->specialization->id);
+                })
+                ->with('doctorProfile')
+                ->groupBy('doctor_profile_id')
+                ->get();
+
+            // if only some doctor have queue
+            if ($queueDoctors->count() != DoctorProfile::where('specialization_id', $this->specialization->id)->count('id')) {
+                // choose one of the missing doctor to be given patient
+                $busyDoctorIds = $queueDoctors->pluck('doctor_profile_id');
+                $freeDoctors = DoctorProfile::where('specialization_id', $this->specialization->id)->whereNotIn('id', $busyDoctorIds)->get();
+                $this->doctor = $freeDoctors->random();
+            } else {
+                // choose the doctor with the least queues_count
+                $this->doctor = $queueDoctors->sortBy('queues_count')->first()->doctorProfile;
+            }
+        } else {
+            $this->locket = Locket::get()->random();
+            $this->previousQueue = CheckUpQueue::where('locket_id', $this->locket->id)->latest()->first();
+            $this->doctor = DoctorProfile::where('specialization_id', $specialization->id)
+                ->get()->random();
+        }
+
+        $isDoctorBusy = CheckUpQueue::where('doctor_profile_id', $this->doctor->id)->exists();
 
         $checkUpQueue = CheckUpQueue::create([
             'patient_id' => $patient->id,
-            'doctor_profile_id' => $doctor->id,
-            'number' => $queue ? ++$queue->number : 1,
-            'locket_id' => $locket->id
+            'doctor_profile_id' => $this->doctor->id,
+            'number' => $this->previousQueue ? ++$this->previousQueue->number : 1,
+            'locket_id' => $this->locket->id
         ]);
 
         if (!$isDoctorBusy) {
             DoctorIsFree::dispatch($doctor);
         }
 
-        return $checkUpQueue->number . $locket->code;
+        return $checkUpQueue->number . $this->locket->code;
     }
 }
