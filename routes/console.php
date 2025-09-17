366047c71d33c268beff77f4317b25ffa097d372
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-09-17 20:45:43 +0700

    feat: schedule daily queue clearing

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\CheckUpQueue;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    CheckUpQueue::truncate();
+})->daily();
