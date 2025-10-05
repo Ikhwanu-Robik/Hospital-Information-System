f4f7bb8335effb7d2638bcefbc34e368aefc98f6
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-10-05 19:24:48 +0700

    add comment for a class

--- app/Events/DoctorIsFree.php
+++ app/Events/DoctorIsFree.php
@@ -11,6 +11,16 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+/**
+ * There are two conditions where DoctorIsFree
+ * is suitable for dispatching.
+ * 
+ * 1. When doctor does not have any queue with waiting patient,
+ *    thus is free to accept a patient
+ * 2. When doctor has a queue with waiting patient, but the doctor
+ *    just finished checking up a patient, thus is free to accept
+ *    another patient
+ */
 class DoctorIsFree
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
