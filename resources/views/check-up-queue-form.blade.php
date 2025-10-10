19fbc215d4793d99040eb5eb4304f888c588d99d
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-10-10 20:32:19 +0700

    optimize

--- resources/views/check-up-queue-form.blade.php
+++ resources/views/check-up-queue-form.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Get Queue Number</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite('resources/js/validationErrorAlert')
+    @vite('resources/js/validationErrorAlert.js')
 </head>
 
 <body class="p-4">
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/autocomplete-custom.css',
+                'resources/css/diagnosis-form.css',
+                'resources/css/locket-page.css',
+                'resources/css/print-medicine-prescriptions.css',
+                'resources/js/app.js',
+                'resources/js/doctorPing.js',
+                'resources/js/listenToPatient.js',
+                'resources/js/listenToQueueBroadcast.js',
+                'resources/js/listenToStripe.js',
+                'resources/js/qzTrayThermalPrint.js',
+                'resources/js/useAutoComplete.js',
+                'resources/js/validateMedicineForm.js',
+                'resources/js/validationErrorAlert.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
