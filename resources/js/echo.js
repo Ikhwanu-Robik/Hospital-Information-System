0b382185c6c8021842ed6b4a496a78af63ecf7ed
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-18 13:10:29 +0700

    fix: pusher initialized without arg

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,11 +1,16 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
-window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
+const options = {
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
+};
+
+window.Pusher = new Pusher(options.key);
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    ...options
 });
