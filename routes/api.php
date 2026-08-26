e52ca03f37a0867804b15372d786304dcd4c3650
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-26 08:28:32 +0700

    fix: event didn't instantiate model from argument

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::post('/stripe-webhook', StripeWebhookController::class)->middleware(Force
 Route::get('/qztray/certificate', [QZTrayPrintingController::class, 'certificate'])->middleware(ForceJsonResponse::class);
 Route::post('/qztray/message/sign', [QZTrayPrintingController::class, 'sign'])->middleware(ForceJsonResponse::class);
 
-Route::get('/medicine-dispense-status/{prescriptionRecordId}', function (\Illuminate\Http\Request $request, string $prescriptionRecordId) {
-    \App\Events\StripePaymentProcessed::dispatch($prescriptionRecordId);
+Route::get('/medicine-dispense-status/{prescriptionRecord}', function (\Illuminate\Http\Request $request, \App\Models\PrescriptionRecord $prescriptionRecord) {
+    \App\Events\StripePaymentProcessed::dispatch($prescriptionRecord);
 });
