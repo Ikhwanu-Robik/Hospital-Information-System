2795e24421efe063ae35c281ec2610d2e7d10a48
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-26 21:14:38 +0700

    add logging in channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -8,30 +8,39 @@ use App\Models\DoctorSchedule;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('CheckUp.Doctors.{doctorProfileId}', function ($user, $doctorProfileId) {
+    logger('fetching the doctor profile by id');
     $doctorProfile = DoctorProfile::find($doctorProfileId);
 
     if (!$doctorProfile) {
+        logger('the id doesn\'t correspond to any doctor profile y\'all');
         return false;
     }
 
+    logger('now()');
     $now = now();
-    
+
+    logger('is doctor in schedule?');
     $isDoctorInSchedule = DoctorSchedule::where('doctor_profile_id', $doctorProfile->id)
         ->where('day_of_week', $now->dayName)
         ->where('start_time', '<=', $now->format('H:i:s'))
         ->where('end_time', '>=', $now->format('H:i:s'))
         ->exists();
 
+    logger('does doctor have an awaiting patient?');
     $isDoctorBusy = CheckUpQueue::where('doctor_profile_id', $doctorProfile->id)
             ->where('status', CheckUpStatus::WAITING->value)
             ->exists();
 
     if ($doctorProfile->user->can('accept patient') && $isDoctorInSchedule) {
+        logger('doctor can acccept patient, shocking. he\'s also in schedule too');
         if ($isDoctorBusy) {
+            logger('he also has an awaiting patient');
             DoctorIsFree::dispatch($doctorProfile);
         }
         return true;
     }
+
+    logger('the doctor doesn\'t have the permission to accept patient, or he\'s just not in schedule');
     return false;
 });
 
