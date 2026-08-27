ce3f7f58b2a1cbe3ef9a07d0b4deb8832a65e5f2
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-27 07:50:36 +0700

    fix: wrong argument

--- app/Http/Controllers/CheckUpController.php
+++ app/Http/Controllers/CheckUpController.php
@@ -151,8 +151,7 @@ class CheckUpController extends Controller
             'doctor_profile_id' => 'required|exists:doctor_profiles,id'
         ]);
 
-        $doctor = DoctorProfile::find($validated['doctor_profile_id']);
-        DoctorIsFree::dispatch($doctor);
+        DoctorIsFree::dispatch($validated['doctor_profile_id']);
     }
 
     public function skipPatient(Request $request)
